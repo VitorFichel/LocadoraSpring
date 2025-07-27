284749fde0e040214b58fc378229f54f390e2eea
VitorFichel <vitorfichel@gmail.com>
2025-07-27 20:08:10 -0300

    feat: Implementação completa de autenticação e autorização com Spring Security, JWT tokens e Roles de ADMIN e USER

---
+++ src/main/java/com/example/locadoraSpring/model/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.example.locadoraSpring.model;
+
+public record LoginResponseDTO(String token) {
+}
---
+++ src/main/java/com/example/locadoraSpring/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.locadoraSpring.repository;
+
+import com.example.locadoraSpring.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
+}
